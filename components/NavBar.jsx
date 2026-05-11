5c1c0c0c12cbbe289706e3ea1984670fb2f46b0b
ruthdolf <pruekzorn@gmail.com>
2026-05-11 15:00:38 +0300

    added background logo

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,7 +2,7 @@ export default function NavBar() {
     return (
         <header className="header">
             <nav>
-                <img className="nav-logo" src='../react-logo.png' alt="React logo"></img>
+                <img className="nav-logo" src='../images/react-logo.png' alt="React logo"></img>
                 <span>React Facts</span>
             </nav>
         </header>
