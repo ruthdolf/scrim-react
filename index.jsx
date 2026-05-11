b8a77c9477895d7d5f91ce501d091c085485c7a5
ruthdolf <pruekzorn@gmail.com>
2026-05-11 12:13:55 +0300

    custom parent/child components for Page-> Header, Main and Footer

--- index.jsx
+++ index.jsx
@@ -5,26 +5,41 @@ root.render(
     <Page />
 )
 
+function Header() {
+    return (
+        <header>
+                <img src="react-logo.png" width="40px" alt="React logo" />
+        </header>
+    )
+}
+
+function Main() {
+    return (
+        <main>
+            <h1>Reasons why I'm excited to learn React</h1>
+            <ol>
+                <li>for my internship</li>
+                <li>I want to make an impactful web app</li>
+                <li>I like to challenge myself</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2026 Lukkanothai development. All rights reserved.</small>
+        </footer>
+    )
+}
+
 function Page() {
     return (
         <>
-            <header>
-                <img src="react-logo.png" width="40px" alt="React logo" />
-            </header>
-
-            <main>
-                
-                <h1>Reasons why I'm excited to learn React</h1>
-                <ol>
-                    <li>for my internship</li>
-                    <li>I want to make an impactful web app</li>
-                    <li>I like to challenge myself</li>
-                </ol>
-            </main>
-
-            <footer>
-                <small>© 2026 Lukkanothai development. All rights reserved.</small>
-            </footer>
+            <Header/>
+            <Main/>
+            <Footer/>
         </>
     )
 }
