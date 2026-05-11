d09daa8eb19114401bdcfb6c7dd872c75d9217a8
ruthdolf <pruekzorn@gmail.com>
2026-05-11 11:54:59 +0300

    custom components

--- index.jsx
+++ index.jsx
@@ -2,15 +2,29 @@ import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
 root.render(
-    <main>
-        <img src="react-logo.png" width="40px" alt="React logo" />
-        <h1>Fun facts about React!</h1>
-        <ul>
-            <li>Was first release in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 200K stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </main>
+    <Page />
 )
+
+function Page() {
+    return (
+        <>
+            <header>
+                <img src="react-logo.png" width="40px" alt="React logo" />
+            </header>
+
+            <main>
+                
+                <h1>Reasons why I'm excited to learn React</h1>
+                <ol>
+                    <li>for my internship</li>
+                    <li>I want to make an impactful web app</li>
+                    <li>I like to challenge myself</li>
+                </ol>
+            </main>
+
+            <footer>
+                <small>© 2026 Lukkanothai development. All rights reserved.</small>
+            </footer>
+        </>
+    )
+}
